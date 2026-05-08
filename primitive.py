205156e9b82bdaf4ec929f380f886c44ed39421e
Ray <firdavsyangiev@gmail.com>
2026-05-09 08:12:07 +0900

    feat: learn booleans in python

--- primitive.py
+++ primitive.py
@@ -24,3 +24,22 @@ print(f"the result (2): {result}")
 
 result = course.replace("Full Stack", "Masterclass")
 print(f"the result (3): {result}")
+
+print("=======booleans=======")
+# function > type() input() bool() int() str()
+y = input("Give your value for y: ")
+print("y:", y)
+
+result = y.isnumeric()
+print(f"the input value is numeric: {result}")
+
+# TRUTHY and FALSY values
+# TRUTHY: True 100 -100 "MIT"
+# FALSY: False 0  "" None
+
+test_falsy = "" or False or 0 or None
+print("The test_falsy:", bool(test_falsy))
+
+test_truthy = "MIT"
+print("test_truthy:", bool(test_truthy))
+1200
