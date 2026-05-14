15eab4b9507f1a6d7e716b9877a1a7bdd2d41d46
Ray <firdavsyangiev@gmail.com>
2026-05-14 15:33:57 +0900

    feat:Inheritance concept

---
+++ class.ip.py
@@ -0,0 +1,93 @@
+'''CLASS DEEP DIVING
+   (1) Encapsulation
+   (2) Inheritance <
+   (3) Polymorphism  <   
+'''
+
+print("=====Inheritance=======")
+
+
+class Animal():
+    # state
+    description = "This class is Parent for animals"
+    # constructor
+
+    def __init__(self, voice):
+        self._status = "This animal is alive!"
+        self.voice = voice
+
+    # method
+    def make_voice(self):
+        print(f"The animal can makevoice: {self.voice}")
+
+
+class Dog(Animal):
+    # state
+
+    # constructor
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
+
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
+
+    def protect(self):
+        print("Yes, I can protect you!")
+
+
+class Cat(Animal):  # Child
+    # state
+
+    # constructor
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
+
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
+
+    def play(self):
+        pass
+
+
+class Fish(Animal):  # Child
+    # state
+
+    # constructor
+    def __init__(self, name, sound, voice):
+        self.name = name
+        self.sound = sound
+        super().__init__(voice)
+
+    # method
+    def introduce(self):
+        print(f"{self.name} says: {self.sound}-{self.sound}")
+
+    def swim(self):
+        print("Yes, I can swim!")
+
+
+dog = Dog("Rex", "woof-woof", True)
+cat = Cat("Tom", "meow-meow", True)
+fish = Fish("Bubbles", "Zzzz", False)
+
+dog.introduce()
+cat.introduce()
+fish.introduce()
+
+print("============")
+dog.make_voice()
+cat.make_voice()
+
+print("============")
+print(Animal.description)
+print(dog.description)
+
+print("============")
+print(dog._status)
+print(cat._status)
